745ccb21bc229ada41c9cf76524fe93438e8fa21
Pablo Rebolini <rebolini.pablo@gmail.com>
2013-09-08 00:14:00 -0300

    Fix: Indentacion

--- DAO_VO/class/class.ClientDAO.php
+++ DAO_VO/class/class.ClientDAO.php
@@ -12,15 +12,15 @@
 			if(!is_int($id)) throw new InvalidArgumentException('Se esperaba: Entero');
 
 			$_sql = "SELECT id, name, contact ".
-				    "FROM clients ".
-				    "WHERE id = {$id}";
+                    "FROM clients ".
+                    "WHERE id = {$id}";
 			return $this->directQuery($_sql);
 		}
 		
 		public function save(ClientVO $client){
 			$_sql = "INSERT INTO `clients` ".
-					"(`id`, `name`, `contact`) ".
-					"VALUES ('{$client->getID()}','{$client->getName()}','{$client->getContact()}')";
+                    "(`id`, `name`, `contact`) ".
+                    "VALUES ('{$client->getID()}','{$client->getName()}','{$client->getContact()}')";
 			
 			return $this->directQuery($_sql);
 		}
--- DAO_VO/main.php
+++ DAO_VO/main.php
@@ -2,32 +2,29 @@
 	/**
 	 * Archivo principal
 	 * Ejemplo de implementacion de DAO + VO
+	 * @author Rebolini Pablo <rebolini.pablo@gmail.com>
 	 *
-	 *
-	 *	Objetivo: Proveer acceso a un modelo sin revelar datos
-	 *			  de su estructura interna.
+	 *	Objetivo:   Proveer acceso a un modelo sin revelar datos
+	 *              de su estructura interna.
 	 *			  
-	 *			  Es independiente del sistema de persistencia. 
-	 *			  Podriamos cambiar el almacenamiento de una base
-	 *			  de datos relacional a una noSQL sin modificar 
-	 *			  el resto de la aplicacion.
+	 *              Es independiente del sistema de persistencia. 
+	 *              Podriamos cambiar el almacenamiento de una base
+	 *              de datos relacional a una noSQL sin modificar 
+	 *              el resto de la aplicacion.
 	 *			  
-	 *	 		  Provee una cantidad determinada de metodos que
-	 *			  nos solucionan el problema de la persistencia de datos.
+	 *              Provee una cantidad determinada de metodos que
+	 *              nos solucionan el problema de la persistencia de datos.
 	 *	
 	 *
 	 *	Aplicacion: El patron DAO puede ser utilizado siempre que se
-	 *				necesite abstraer el acceso a la persistencia.
-	 *				Dentro de un framework MVC un Modelo bien 
-	 *				puede ser un DAO.
-	 *				El patron DAO crea una capa de abstraccion que no 
-	 *				siempre es util
-	 *				El patron DAO se puede, y en algunos casos se debe,
-	 *				combinar con el patron VO, DTO, Factory, Repository.
-	 *				Aunque no existen reglas de oro para esto.	
-	 *				
-	 *
-	 * @author Rebolini Pablo <rebolini.pablo@gmail.com>
+	 *              necesite abstraer el acceso a la persistencia.
+	 *              Dentro de un framework MVC un Modelo bien 
+	 *              puede ser un DAO.
+	 *              El patron DAO crea una capa de abstraccion que no 
+	 *              siempre es util
+	 *              El patron DAO se puede, y en algunos casos se debe,
+	 *              combinar con el patron VO, DTO, Factory, Repository.
+	 *              Aunque no existen reglas de oro para esto.	
 	 **/
 	
 	// Array de configuracion
